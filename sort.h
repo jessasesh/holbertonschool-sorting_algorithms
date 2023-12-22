5ac84091b5320835dda6ccbeef70bd6691295020
jessasesh <jessica.dison@yahoo.com>
2023-12-22 12:22:23 -0800

    Forgotten commits?

--- sort.h
+++ sort.h
@@ -30,5 +30,8 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void swp(int *firel, int *secel);
+int lomuto_partition(int *array, int low, int high, size_t size);
+void qs_recur(int *array, int low, int high, size_t size);
 
 #endif
