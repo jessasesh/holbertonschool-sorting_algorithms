6011fc22b740ed082a101939a4f81fa1568be538
jessasesh <jessica.dison@yahoo.com>
2023-12-22 12:26:37 -0800

    Fixed for loop T3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -22,8 +22,9 @@ int lomuto_partition(int *array, int low, int high, size_t size)
 {
 	int pivot = array[high];
 	int i = low - 1;
+	int j;
 
-	for (int j = low; j <= high - 1; j++)
+	for (j = low; j <= high - 1; j++)
 	{
 		if (array[j] <= pivot)
 		{
